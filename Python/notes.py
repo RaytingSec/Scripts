dd4b1b59ebcf9cb956c2163badc2cf755487a03e
raytingsec <rayting@raydar.net>
2016-09-15 19:46:55 -0700

    notes.py opens subl, symlinks for firstboot

--- Python/notes.py
+++ Python/notes.py
@@ -1,6 +1,7 @@
 # Create template notes txt file
 
 import datetime
+from subprocess import call
 
 date = str(datetime.datetime.now().date())
 
@@ -8,5 +9,9 @@ result = date + '\n'
 result += '=' * 30 + '\n\n'
 result += '## '
 
-with open(date, "w") as file:
+filename = "{}.md".format(date)
+
+with open(filename, "w") as file:
     file.write(result)
+
+call(["subl", filename])
