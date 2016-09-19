04ee2341a3eb2d876b460e847b1b3eaf030f46d9
raytingsec <rayting@raydar.net>
2016-09-19 15:04:43 -0700

    filename bug fix

--- Python/notes.py
+++ Python/notes.py
@@ -4,10 +4,9 @@ import datetime
 from subprocess import call
 import sys
 
+title = ""
 if len(sys.argv) > 1:
     title = sys.argv[1]
-else:
-    title = ""
 
 date = str(datetime.datetime.now().date())
 
@@ -15,7 +14,9 @@ result = "{}\n{}\n".format(date, title)
 result += '=' * 30 + '\n\n'
 result += '## '
 
-filename = "{} {}".format(date, title)
+filename = "{}".format(date)
+if len(sys.argv) > 1:
+    filename = "{} {}".format(date, title)
 
 with open(filename, "w") as file:
     file.write(result)
