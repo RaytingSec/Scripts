0cfbc5ed499040833f69ff039bd3ad2679854bab
raytingsec <rayting@raydar.net>
2017-03-22 19:46:25 -0700

    fix: formatting issues

--- Python/msu.py
+++ Python/msu.py
@@ -7,13 +7,13 @@ import random
 randint = random.SystemRandom().randint
 
 time = '{}:{:02}'.format(randint(20, 23), randint(0, 59))
-pace = 6 * 60 + 30 + randint(0, 59)
+pace = 6 * 60 + 45 + randint(0, 59)
 distance = round(1.8 + (randint(0, 30) / 100), 2)
 duration = pace * distance
 
 # Stringify
-pace = '{}:{:02}'.format(int(pace / 60), int(pace % 60))
-duration = '{}:{:02}'.format(int(duration / 60), int(duration % 60))
+pace = '{:02}:{:02}'.format(int(pace / 60), int(pace % 60))
+duration = '{:02}:{:02}'.format(int(duration / 60), int(duration % 60))
 
 day = input('Choose a day this record occurs on (yyyy-mm-dd format): ')
 print('{}\t{}\t{}\t{}\t{}\t'.format(day, time, distance, duration, pace))
