bf8a4fae6734f6464a29a700e04a5b58f8c019f1
raytingsec <rayting@raydar.net>
2019-05-27 23:15:49 -0700

    fix: suspend command has changed over time

--- Bash/suspend.sh
+++ Bash/suspend.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Get root permissions
 # sudo pwd > /dev/null
-sleep 1 && sudo pm-suspend &
-gnome-screensaver-command --lock
+
+# # Using pm
+# sleep 1 && sudo pm-suspend &
+# # In Gnome 3.28, screensaver is no longer built in
+# # gnome-screensaver-command --lock
+# dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
+
+# New method using systemd
+systemctl -i suspend
+# Shouldn't need to run lock command as machine should lock itself on suspend
